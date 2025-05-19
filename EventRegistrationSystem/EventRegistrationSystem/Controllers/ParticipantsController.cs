46940cf8a349ace16f39708326de36471f6ec448
Michal <michal.siudut.ms@gmail.com>
2025-05-19 18:04:53 +0200

    adding participants added

--- EventRegistrationSystem/EventRegistrationSystem/Controllers/ParticipantsController.cs
+++ EventRegistrationSystem/EventRegistrationSystem/Controllers/ParticipantsController.cs
@@ -13,6 +13,27 @@ namespace EventRegistrationSystem.Controllers
         {
             return View(InMemoryDatabase.Participants);
         }
+        [HttpPost]
+        public IActionResult CreateAParticipant(ParticipantModel participant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", participant);
+            }
+            participant.Id = InMemoryDatabase.Participants.Count > 0
+                ? InMemoryDatabase.Participants.Max(p => p.Id) + 1
+                : 1;
+
+            InMemoryDatabase.Participants.Add(participant);
+
+            return RedirectToAction("Create");
+        }
+        [HttpGet]
+        public IActionResult CreateAParticipant()
+        {
+            return View();
+        }
+
         public IActionResult ListByEvent()
         {
             return View();
